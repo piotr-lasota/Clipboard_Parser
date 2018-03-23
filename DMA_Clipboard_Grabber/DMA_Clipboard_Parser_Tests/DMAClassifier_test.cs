8418003c31ab060884a94cb84205aae2cb9dc0cd
Piotr Lasota <lasota.piotr@live.com>
2018-03-23 19:19:37 +0100

    TestsProject

---
+++ DMA_Clipboard_Grabber/DMA_Clipboard_Parser_Tests/DMAClassifier_test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DMA_Clipboard_Parser_Tests
+{
+    [TestClass]
+    public class DMAClassifier_test
+    {
+        [TestMethod]
+        public void Classifier()
+        {
+        }
+    }
+}
---
+++ DMA_Clipboard_Grabber/DMA_Clipboard_Parser_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DMA_Clipboard_Parser_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DMA_Clipboard_Parser_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71395f80-fce2-424c-801d-dae405970746")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
