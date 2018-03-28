bb176c083d1a39c40bee72e2afd6703f34a77713
Piotr Lasota <lasota.piotr@live.com>
2018-03-28 22:32:05 +0200

    ZeroFiller helper class with tests

--- DMA_Clipboard_Grabber/DMA_Clipboard_Grabber/Helpers/ZeroFiller.cs
+++ DMA_Clipboard_Grabber/DMA_Clipboard_Grabber/Helpers/ZeroFiller.cs
@@ -8,22 +8,29 @@ namespace DMA_Clipboard_Grabber
 {
     public static class ZeroFiller
     {
-        public static string fillToDigits(string code, int digits, char desiredFill)
+        /// <summary>
+        /// Replaces the asterisk in string with a given character enough times to match a desired length
+        /// </summary>
+        /// <param name="code">Input string</param>
+        /// <param name="desiredLength">Desired length of the string</param>
+        /// <param name="desiredFill">Character to fill up with</param>
+        /// /// <returns></returns>
+        public static string fillToDigits(string code, int desiredLength, char desiredFill)
         {
             // if the code is already at it's desired length, return it back switching it to capital letters.
-            if(code.Length == digits)
+            if(code.Length == desiredLength)
             {
                 return code.ToUpper();
             }
 
             // if the code exceeds it's desired length, throw a FormatException
-            if(code.Length >= digits)
+            if(code.Length >= desiredLength)
             {
                 throw new FormatException("Code is longer than expected. Truncating is not supported");
             }
 
             // Upperchar the string and fill it with the desired character in place of the asterisk.
-            return code.ToUpper().Replace("*", new string(desiredFill, (digits - code.Length + 1)));
+            return code.ToUpper().Replace("*", new string(desiredFill, (desiredLength - code.Length + 1)));
         }
     }
 }
