7956fe2806fc177a119af2fe7ea376cbc2a1c4be
Piotr Lasota <lasota.piotr@live.com>
2018-03-28 22:25:27 +0200

    Addid ZeroFiller helper class with tests

---
+++ DMA_Clipboard_Grabber/DMA_Clipboard_Grabber/Models/Classifier/ZeroFiller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMA_Clipboard_Grabber
+{
+    public static class ZeroFiller
+    {
+        public static string fillToDigits(string code, int digits, char desiredFill)
+        {
+            // if the code is already at it's desired length, return it back switching it to capital letters.
+            if(code.Length == digits)
+            {
+                return code.ToUpper();
+            }
+
+            // if the code exceeds it's desired length, throw a FormatException
+            if(code.Length >= digits)
+            {
+                throw new FormatException("Code is longer than expected. Truncating is not supported");
+            }
+
+            // Upperchar the string and fill it with the desired character in place of the asterisk.
+            return code.ToUpper().Replace("*", new string(desiredFill, (digits - code.Length + 1)));
+        }
+    }
+}
---
+++ DMA_Clipboard_Grabber/DMA_Clipboard_Parser_Tests/ZeroFiller_tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DMA_Clipboard_Grabber;
+
+namespace DMA_Clipboard_Parser_Tests
+{
+    [TestClass]
+    public class ZeroFiller_tests
+    {
+        [TestMethod]
+        public void ZeroFiller_ShortCode_ShouldBeFilled()
+        {
+            // arrange
+            string input = "AB*7890";
+            string expectedOutput = "AB00000007890";
+
+            // act
+            string output = ZeroFiller.fillToDigits(input, 13, '0');
+            // assert
+            Assert.AreEqual<String>(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void ZeroFiller_SmallLetterShortCode_ShouldBeCapitalized()
+        {
+            // arrange
+            string input = "ab0*7890";
+            string expectedOutput = "AB00000007890";
+
+            // act
+            string output = ZeroFiller.fillToDigits(input, 13, '0');
+            // assert
+            Assert.AreEqual<String>(expectedOutput, output);
+        }
+
+        [TestMethod]
+        public void ZeroFiller_SmallLetterFullCode_ShouldBeCapitalized()
+        {
+            // arrange
+            string input = "ab00000007890";
+            string expectedOutput = "AB00000007890";
+
+            // act
+            string output = ZeroFiller.fillToDigits(input, 13, '0');
+            // assert
+            Assert.AreEqual<String>(expectedOutput, output);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void ZeroFiller_TooLongInput_ShouldThrowFormatException()
+        {
+            // arrange
+            string input = "AB000000007890"; // one too many
+
+            // act
+            string output = ZeroFiller.fillToDigits(input, 13, '0');
+            
+            // assert handled by ExpectedException
+        }
+    }
+}
