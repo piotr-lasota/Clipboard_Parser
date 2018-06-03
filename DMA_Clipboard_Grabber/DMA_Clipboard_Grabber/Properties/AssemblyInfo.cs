e7f44a3c5c39e9c0de4d16438e4d63cbd232378e
Piotr Lasota <lasota.piotr@live.com>
2018-06-03 21:08:27 +0200

    Tinkering

--- DMA_Clipboard_Grabber/DMA_Clipboard_Grabber/Properties/AssemblyInfo.cs
+++ DMA_Clipboard_Grabber/DMA_Clipboard_Grabber/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DMA_Clipboard_Grabber")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Clipboard codes parser")]
+[assembly: AssemblyDescription("Handy tool for finding DMA codes in your clipboard")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Piotr Lasota")]
 [assembly: AssemblyProduct("DMA_Clipboard_Grabber")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyleft  2018")]
+[assembly: AssemblyTrademark("Piotr Lasota")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -51,5 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
